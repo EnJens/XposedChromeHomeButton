0448c0568d85c704edad7635283dd26e7344ce29
Jens Andersen <jens.andersen@gmail.com>
2017-10-28 17:01:26 +0200

    Hook onClick handler to open our settings view on home button click

--- app/src/main/java/dk/enjens/xposedchromedemo/LoadHook.java
+++ app/src/main/java/dk/enjens/xposedchromedemo/LoadHook.java
@@ -1,30 +1,35 @@
 package dk.enjens.xposedchromedemo;
 
-import android.graphics.Bitmap;
+import android.app.AndroidAppHelper;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.graphics.Color;
-import android.widget.FrameLayout;
+import android.view.View;
 import android.widget.ImageButton;
-import android.widget.TextView;
-
-import java.lang.reflect.Method;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 public class LoadHook implements IXposedHookLoadPackage {
+    ImageButton homeButton;
+
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
         if(!lpparam.packageName.equals("com.android.chrome"))
             return;
         XposedBridge.log("Loaded app: " + lpparam.packageName);
         Class<?> tintedImgButton = findClass("org.chromium.chrome.browser.widget.TintedImageButton", lpparam.classLoader);
+        Class<?> toolbarPhone = findClass("org.chromium.chrome.browser.toolbar.ToolbarPhone", lpparam.classLoader);
 
-        findAndHookMethod("org.chromium.chrome.browser.toolbar.ToolbarPhone", lpparam.classLoader, "onFinishInflate", new XC_MethodHook() {
+        findAndHookMethod(toolbarPhone, "onFinishInflate", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 // this will be called before the clock was updated by the original method
@@ -32,9 +37,32 @@ public class LoadHook implements IXposedHookLoadPackage {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 XposedBridge.log("in onFinishInflate");
-                ImageButton obj = (ImageButton)getObjectField(param.thisObject, "mHomeButton");
-                obj.setBackgroundColor(Color.RED);
-                obj.setAlpha(1.0f);
+                homeButton = (ImageButton)getObjectField(param.thisObject, "mHomeButton");
+                homeButton.setBackgroundColor(Color.RED);
+                homeButton.setAlpha(0.2f);
+            }
+        });
+
+        findAndHookMethod(toolbarPhone, "onClick", View.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("in onClick");
+                View v = (View)param.args[0];
+                if(homeButton != null && v == homeButton)
+                {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    // We don't have access to any useful context here, so hardcode our known exported activity
+                    ComponentName component = ComponentName.unflattenFromString("dk.enjens.xposedchromedemo/dk.enjens.xposedchromedemo.SettingsActivity");
+                    if(component == null)  {
+                        XposedBridge.log("Component == null!?");
+                        return;
+                    }
+                    intent.setComponent(component);
+                    AndroidAppHelper.currentApplication().startActivity(intent);
+                }
             }
         });
     }
